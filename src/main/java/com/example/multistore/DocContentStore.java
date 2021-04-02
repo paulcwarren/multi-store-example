fe4a1127425c14c045b4ed7c41ecb58007c102d9
Paul Warren <warrenpa@vmware.com>
2021-04-01 21:15:08 -0700

    Make storage modules runtime dependencies

--- src/main/java/com/example/multistore/DocContentStore.java
+++ src/main/java/com/example/multistore/DocContentStore.java
@@ -2,9 +2,7 @@ package com.example.multistore;
 
 import java.util.UUID;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.content.commons.repository.ContentStore;
-import org.springframework.content.fs.store.FilesystemContentStore;
 
 public interface DocContentStore extends ContentStore<Doc, UUID> {
 
